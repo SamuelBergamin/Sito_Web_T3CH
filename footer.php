b3e3b569306f005cc6ad074f4c47d1204dfc83e5
SamuelBergamin <bergamin.samuel@gmail.com>
2018-06-08 16:01:17 +0200

    Initial commit

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer>
+	<div class="text-center">
+		<em>/// T3CH /// All rights reserved.</em>
+	</div>
+</footer>
+</html>
---
+++ index.php
@@ -0,0 +1,148 @@
+<?php
+	include 'header.php';
+?>
+
+<header style="background-color:#353535;">
+  <a href="index_en.php">Passa a Inglese</a>
+  <div class="text-center">
+    <img src="images/logo.png">
+    <h1>Questo è il mio sito!</h1>
+  </div>
+</header>
+<body style="background-color:#353535;">
+  
+    <div class="container">
+    	<div class="row">
+    		<div class="col-sm-12">
+    		&nbsp
+    			<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+				  <ol class="carousel-indicators">
+				    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+				    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+				    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+				  </ol>
+				  <div class="carousel-inner">
+				    <div class="carousel-item active">
+				      <img class="d-block w-100" src="images/lockscreen.png" alt="First slide">
+				    </div>
+				    <div class="carousel-item">
+				      <img class="d-block w-100" src="images/colours.png" alt="Second slide">
+				    </div>
+				    <div class="carousel-item">
+				      <img class="d-block w-100" src="images/boh.png" alt="Third slide">
+				    </div>
+				  </div>
+				  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				    <span class="sr-only">Precedente</span>
+				  </a>
+				  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+				    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+				    <span class="sr-only">Successiva</span>
+				  </a>
+				</div>
+
+    		</div>
+    	</div>
+    	&nbsp
+      <div class="row">
+        <div class=col-md-4>
+          <h2>Info</h2>
+          <p>
+            <ul>
+              <li>Età: 17</li>
+              <li>Professione: Studente</li>
+              <li>Passione: Hardware, Grafica, Programmazione...</li>
+              <li>Professional CS:GO Player</li>
+              <li>Amante dei videogiochi</li>
+	        </ul>
+	       </p>
+	        </div>
+	        <div class=col-md-4>
+	        <h2>PC RIG</h2>
+	          <p>
+	          	<ul>
+	          		<li>CASE: Cooler Master Masterbox 5</li>
+	          		<li>MOBO: MSI H270 Gaming M3</li>
+	          		<li>CPU: i5 7400 kaby lake</li>
+	          		<li>HEATSINK: NZXT Kraken x42</li>
+	          		<li>RAM: G.Skill Trident Z RGB 16GB DDR4 2400Mhz</li>
+	          		<li>GPU: MSI GTX 1050ti oc 4GB (doppia ventola)</li>
+	          		<li>PSU: EVGA 600w</li>
+	          		<li>HDD: WD Black 1tb</li>
+	          		<li>Keyboard: Razer Ornata CHROMA</li>
+	          		<li>Mouse: BenQ ZOWIE EC2-B</li>
+	          		<li>MAT: Steelseries QCK+</li>
+	          		<li>Monitor: Asus VX228H FHD 21,5" 1ms {2017}</li>
+	          		<li>Headset: HyperX Cloud Alpha</li>
+	          	</ul>
+	          </p>
+	        </div>
+	        <div class=col-md-4>
+	        <h2>IN-GAME INFO</h2>
+	          <p>
+	          	<ul>
+	          		<li>ID: >_T3CH </li>
+	          		<li>Livello: 16</li>
+	          		<li>Prestigio: 5</li>
+	          		<li>Rank MM: Capo della Guardia Elite</li>
+	          		<li>Rank WingMan: Capo della Guardia II</li>
+	          		<li>Ore di servizio: 2694 ore</li>
+	          	</ul>
+	          </p>
+	        </div>
+	      </div>
+	      &nbsp
+	      <div class="row">
+	        <div class="col-md-6">
+	          <p class="text-center">
+	            <a class="btn btn-primary" data-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Crosshair</a>
+	          </p>
+	            <div class="collapse multi-collapse" id="multiCollapseExample1" style="font-family: unisanslight">
+	              <div class="card card-body">
+	                <li style="color: #353535">cl_crosshairstyle 4</li>
+	                <li style="color: #353535">cl_crosshairsize 3</li>
+	                <li style="color: #353535">cl_crosshairthickness 0.5</li>
+	                <li style="color: #353535">cl_crosshairgap -2.5</li>
+	                <li style="color: #353535">cl_crosshair_drawoutline 0</li>
+	                <li style="color: #353535">cl_crosshair_outlinethickness 0.5</li>
+	                <li style="color: #353535">cl_crosshairdot 0</li>
+	                <li style="color: #353535">cl_crosshair_t 0</li>
+	                <li style="color: #353535">cl_crosshaircolor 5</li>
+	                <li style="color: #353535">cl_crosshaircolor_r 0</li>
+	                <li style="color: #353535">cl_crosshaircolor_g 255</li>
+	                <li style="color: #353535">cl_crosshaircolor_b 0</li>
+	                <li style="color: #353535">cl_crosshairalpha 255</li>
+	              </div>
+	            </div>
+	          </div>
+	          <div class="col-md-6">
+	            <p class="text-center">
+	              <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">Viewmodel</button>
+	            </p>
+	            <div class="collapse multi-collapse" id="multiCollapseExample2">
+	              <div class="card card-body" style="font-family: unisanslight">
+	              	<li style="color: #353535">cl_viewmodel_shift_left_amt "0.500000"</li>
+	              	<li style="color: #353535">cl_viewmodel_shift_right_amt "0.250000"</li>
+	              	<li style="color: #353535">viewmodel_fov "68"</li>
+	              	<li style="color: #353535">viewmodel_offset_x "2.5"</li>
+	              	<li style="color: #353535">viewmodel_offset_y "2.0"</li>
+	              	<li style="color: #353535">viewmodel_offset_z "-2.0"</li>
+	              	<li style="color: #353535">viewmodel_presetpos "0"</li>
+	              	<li style="color: #353535">viewmodel_recoil "1.0"</li>
+	              	<li style="color: #353535">cl_bob_lower_amt "5.000000"</li>
+	              	<li style="color: #353535">cl_bobamt_lat "0.100000"</li>
+	              	<li style="color: #353535">cl_bobamt_vert "0.100000"</li>
+	              	<li style="color: #353535">cl_bobcycle "0.98"</li>
+	              </div>
+	            </div>
+	        </div>
+        </div>
+      </div> 
+    </div>
+</body>
+&nbsp
+
+<?php
+	include 'footer.php';
+?>
